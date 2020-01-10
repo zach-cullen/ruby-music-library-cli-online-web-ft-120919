c0d354bc71e07b4fd2a25278122f57cc5a266fda
Zach Cullen <zacullen@gmail.com>
2020-01-10 20:21:40 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -11,7 +11,6 @@ class MusicLibraryController
     puts "Welcome to your music library!"
     user_input = nil
     
-    until user_input == "exit"
       puts "To list all of the artists in your library, enter 'list artists'."
       puts "To list all of your songs, enter 'list songs'."
       puts "To list all of the genres in your library, enter 'list genres'."
@@ -20,23 +19,11 @@ class MusicLibraryController
       puts "To play a song, enter 'play song'."
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
+    
+    until user_input == "exit"
+      
       user_input = gets.strip
     end 
-    
-    case user_input
-      when "list artists"
-        puts "listing artists"
-      when "list songs"
-        puts "listing songs"
-      when "list genres"
-        puts "listing genres"
-      when "list artist"
-        puts "listing artist"
-      when "list genre"
-        puts "listing genre"
-      else
-        puts "huh?"
-    end
     
   end
 end
