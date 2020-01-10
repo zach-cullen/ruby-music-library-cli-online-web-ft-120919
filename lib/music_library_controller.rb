fa5dc42d66572f81ab89d6aff9636fcc5352c8f7
Zach Cullen <zacullen@gmail.com>
2020-01-10 21:09:34 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -56,7 +56,7 @@ class MusicLibraryController
   end
   
   def list_songs_by_genre
-    puts "Please enter the name of an artist:"
+    puts "Please enter the name of a genre:"
     input = gets.strip
     genre = Genre.find_by_name(input)
     if genre 
