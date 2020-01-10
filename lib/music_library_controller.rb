8f8c214e427ab0c9290d84f638d68d4f0bff7c0f
Zach Cullen <zacullen@gmail.com>
2020-01-10 20:55:02 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -27,7 +27,7 @@ class MusicLibraryController
   end
   
   def list_songs
-    Song.all.sort {|a, b| a.name <=> b.name}.each_with_index(1) do  |song, i| 
+    Song.all.sort {|a, b| a.name <=> b.name}.each.with_index(1) do  |song, i| 
       puts "#{i + 1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
     end
   end
